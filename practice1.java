a5c86490baf62fb75d24e7ed462169e17e5d6ff4
Pasindu Madhushan <pasinduwijenayaka123@gmail.com>
2023-09-26 00:28:46 +0530

    Second commit

---
+++ practice1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class practice1 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        
+        String str1 = "Hello pasindu";
+        String str2 = "Welcome to Dep-11";
+        String str3 = "We are born to code";
+        String str;
+        do{ 
+           
+        }
+           
+            }
+            for(int j=0;j<str.length()+3;j++){
+
+            }
+        }
+    }
+    
+}
